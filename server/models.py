c5c907ba0e74e1effc5ee4faddd076e23c98ef11
Ernest Kamau <1.kamauernest@gmail.com>
2025-06-18 18:14:50 +0300

    done

--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Integer)
+    image =db.Column(db.String)
+    price = db.Column(db.Float)
+    
+    def __repr__(self):
+        return f'<Plant {self.id} ,{self.name}, {self.price}>'
